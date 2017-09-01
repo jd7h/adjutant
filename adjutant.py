27691ce0455c3bad7b8d7b8b68dfbc32d3b2f491
Judith van Stegeren <git@jd7h.com>
2017-09-01 19:03:56 +0200

    Remove stray testing code, fix whitespace errors

--- adjutant.py
+++ adjutant.py
@@ -16,7 +16,7 @@ TIMEOUT = 60 #seconds
 
 def get_webpage(url):
     """Obtain response (conn) from a url
-    
+
     url -- url of the form "https://www.jd7h.com"
     """
     logging.debug("Request for %s", url)
@@ -67,7 +67,7 @@ def getcompletedomain(url):
 
 def find_links(url,html,only_internal):
     """Find all links in the html of a webpage. Returns a list with links.
-    
+
     url -- a URL which will be used for matching found links to domain
     html -- Decoded HTML of this URL
     only_internal -- If this flag is set, this function only returns links within the same domain.
@@ -100,7 +100,7 @@ def make_wordlist(text):
     Optimized for text, not special text like code, emailaddresses, etc."""
     return word_tokenize(text.lower()) # word_tokenize from nltk
     # dit gaat nog fout voor emailadressen
-    
+
 def count_words(words_from_content):
     """Takes a list of words and makes a dictionary where the keys are words 
     and the values are integers, ie. frequency of occurrence."""
@@ -114,80 +114,17 @@ def count_words(words_from_content):
 
 def remove_common_words(counted_wordlist, common_words):
     """Given a list of stopwords, removes those stopwords from the frequence dictionary counted_wordlist
-    Stopwords source could be stopwords.words from nltk.corpus."""
+    Stopwords source could be stopwords.words from nltk.corpus.
+    For example: stopwords = stopwords.words('english') """
     for word in common_words:
         if word in counted_wordlist.keys():
             counted_wordlist.pop(word)
     return counted_wordlist
 
-stopwords = stopwords.words('english') + stopwords.words('dutch')
-
 def sort_wordlist(counted_wordlist):
     """Given a dictionary of words (keys) and frequency (values), return a list
     of words,frequency tuples sorted by frequency, most frequent first."""
     return sorted(counted_wordlist.items(), key=lambda x: x[1], reverse=True)
-
-print(stopwords)
-dictwords = sort_wordlist(remove_common_words(count_words(wordlist),stopwords))
-print(dictwords)
-
-
-# In[10]:
-
-
-totalwordlist = []
-totalwordlist.append(wordlist)
-THRESHOLD = 50
-scrapelist = [link for link in scrapelist if ".jpg" not in link]
-# todo make scrape() function
-if len(scrapelist) > THRESHOLD :
-    scrapelist = scrapelist[:THRESHOLD]
-for link in scrapelist:
-    print(link)
-    try:
-        connection = get_webpage(link)
-        htmlcode = extract_html(connection)
-        content = html2plaintext(htmlcode)
-    except Exception as e:
-        continue
-    totalwordlist.append(make_wordlist(content))
-print(totalwordlist)
-
-
-# In[11]:
-
-
-flattotalwordlist = [word for wordlist in totalwordlist for word in wordlist]
-dictwords = sort_wordlist(remove_common_words(count_words(flattotalwordlist),stopwords))
-dictwordsfiltered = [d for d in dictwords if len(d[0])>=3 and d[0].isalpha()] # only long words
-#dictwordsfiltered = [d for d in dictwords if d[0].isalpha()] # only real words
-for i in dictwordsfiltered[:51]:
-    print(i)
-
-
-# In[12]:
-
-
-#print(hoffmanndict)
-#print(foxdict)
-hoffmann = [word[0] for word in hoffmanndict]
-fox = [word[0] for word in foxdict]
-intersect = [word[0] for word in hoffmanndict if word[0] in fox]
-print(intersect)
-print()
-print([word for word in hoffmann if word not in intersect][:25])
-print()
-print([word for word in fox if word not in intersect][:25])
-
-
-# In[ ]:
-
-
-
-
-
-# In[61]:
-
 
 def crawl(starturl,depth,internal,processed):
     content = []
@@ -214,89 +151,27 @@ def crawl(starturl,depth,internal,processed):
                 processed = list(set(processed + processed2))
                 content = content + content2
         return processed, content
-    
-prlist, contentlist = crawl(url,5,True,[])
 
+def test_crawl(url,depth,only_internal):
+    # crawl
+    prlist, contentlist = crawl(url,depth,only_internal,[])
+    print(len(prlist), len(contentlist))
+    # merge all source texts
+    text = ""
+    for html in contentlist:
+        text = text + html2plaintext(html)
+    print(len(text))
+    print(text[:100])
+    # extract and count tokens
+    wordlist = make_wordlist(text)
+    print(wordlist[:100])
+    stopwordset = stopwords.words('english') + stopwords.words('dutch')
+    dictwords = sort_wordlist(remove_common_words(count_words(wordlist),stopwordset))
+    # filter
+    dictwordsfiltered = [d for d in dictwords if len(d[0])>=3 and d[0].isalpha()] # only long words
+    # report
+    for i in dictwordsfiltered[:51]:
+        print(i)
 
-# In[62]:
-
-
-print(len(prlist), len(contentlist))
-
-
-# In[63]:
-
-
-text = ""
-for html in contentlist:
-    text = text + html2plaintext(html)
-
-
-# In[64]:
-
-
-len(text)
-
-
-# In[65]:
-
-
-text[:100]
-
-
-# In[66]:
-
-
-wordlist = make_wordlist(text)
-print(wordlist)
-
-
-# In[67]:
-
-
-dictwords = sort_wordlist(remove_common_words(count_words(wordlist),stopwords))
-dictwordsfiltered = [d for d in dictwords if len(d[0])>=3 and d[0].isalpha()] # only long words
-#dictwordsfiltered = [d for d in dictwords if d[0].isalpha()] # only real words
-for i in dictwordsfiltered[:51]:
-    print(i)
-
-
-# In[ ]:
-
-
-url = "http://github.com"
-
-conn = get_webpage(url)
-print(conn)
-print(conn.geturl())
-
-
-html = extract_html(conn)
-print(html)
-print(getdomain(url))
-host = getcompletedomain(url)
-print(host)
-linklist = find_links(url,html,False)
-for link in linklist:
-    print(link, getdomain(link), getdomain(link) in host)
-
-# beter met scrapy
-
-scrapelist = list(set([link for link in linklist if getdomain(link) in host]))
-#scrapelist = list(set([link for link in linklist if urlparse(link).netloc in host]))
-for link in scrapelist:
-    #print(link)
+if __name__ == "__main__":
     pass
-    
-scrapelist2 = find_links(url,html,True)
-#print(scrapelist2)
-difference = [link for link in scrapelist2 if link not in scrapelist]
-print(difference)
-difference = [link for link in scrapelist if link not in scrapelist2]
-print(difference)
-
-text = html2plaintext(html)
-print(text)
-wordlist = make_wordlist(text)
-print(wordlist)
-
